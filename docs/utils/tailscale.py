76816ea6eb45958cf02a50e08be1e4f52a67ce66
mattuu0 <96681413+mattuu0@users.noreply.github.com>
2025-03-28 10:58:53 +0900

    change tailscale script

--- docs/utils/tailscale.py
+++ docs/utils/tailscale.py
@@ -6,39 +6,24 @@ os.system("apt install -y curl")
 os.system("curl -fsSL https://tailscale.com/install.sh | sh")
 os.system("systemctl enable tailscaled")
 
-original_path = "/etc/systemd/system/multi-user.target.wants/tailscaled.service"
-backup_path = "/etc/systemd/system/multi-user.target.wants/tailscaled.service.org"
+original_path = "/etc/default/tailscaled"
+backup_path = "/etc/default/tailscaled.org"
 
 if not os.path.exists(backup_path):
     # バックアップが存在しないとき
     shutil.copyfile(original_path,backup_path)
     pass
 
-with open("/etc/systemd/system/multi-user.target.wants/tailscaled.service","w",encoding="utf-8") as writeScript:
+with open(original_path,"w",encoding="utf-8") as writeScript:
     writeScript.write("""
-[Unit]
-Description=Tailscale node agent
-Documentation=https://tailscale.com/kb/
-Wants=network-pre.target
-After=network-pre.target NetworkManager.service systemd-resolved.service
+# Set the port to listen on for incoming VPN packets.
+# Remote nodes will automatically be informed about the new port number,
+# but you might want to configure this in order to set external firewall
+# settings.
+PORT="41641"
 
-[Service]
-EnvironmentFile=/etc/default/tailscaled
-ExecStart=/usr/sbin/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 --state=/var/lib/tailscale/tailscaled.state --socket=/run/tailscale/tailscaled.sock --port=${PORT} $FLAGS
-ExecStopPost=/usr/sbin/tailscaled --cleanup
-
-Restart=on-failure
-
-RuntimeDirectory=tailscale
-RuntimeDirectoryMode=0755
-StateDirectory=tailscale
-StateDirectoryMode=0700
-CacheDirectory=tailscale
-CacheDirectoryMode=0750
-Type=notify
-
-[Install]
-WantedBy=multi-user.target
+# Extra flags you might want to pass to tailscaled.
+FLAGS="--tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 --state=/var/lib/tailscale/tailscaled.state --socket=/run/tailscale/tailscaled.sock"
 """)
     
 os.system("systemctl daemon-reload")
